6b03f1a3eab82b5f3f7c37c3b98c490f6d047d6c
“xiaoxiong88103” <xiaoxiong88103@hotmail.com>
2024-02-23 16:38:08 +0800

    完善dockerfile和shell

--- build/build.sh
+++ build/build.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
+# 如果没有提供架构参数，则默认为 amd64
+ARCH="${1:-amd64}"
+
+# 构建 Docker 镜像
 docker build -t gotrace:build .
 
-mkdir ./data
-echo "cd /GoTrace && go build GOOS=linux GOARCH="$($1) + "-o GoTrace . && cp -arf ./config /data && cp GoTrace /data ">> ./data/build.sh
+# 创建一个用于存储数据的目录
+mkdir -p ./data
+
+# 在 data 目录中创建构建脚本
+echo "cd /GoTrace && GOOS=linux GOARCH=${ARCH} go build -o GoTrace . && cp -arf ./config /data && cp GoTrace /data" > ./data/build.sh
 chmod +x ./data/build.sh
+
+# 使用构建的镜像在 Docker 容器中运行
 docker run -itd --rm -v $(pwd)/data:/data --net=host gotrace:build
 
+# 清理工作
 docker rmi -f gotrace:build
 rm -rf ./data/build.sh
-echo "恭喜你编译成功在./data目录下就是编译好的二进制文件和config对应的"
+
+# 打印成功消息
+echo "恭喜！编译成功。在 ./data 目录中找到二进制文件和相应的配置文件。"
