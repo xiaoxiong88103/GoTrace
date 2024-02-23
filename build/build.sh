ee56f8b4947eacd9c1869e7633c657f295733d5a
“xiaoxiong88103” <xiaoxiong88103@hotmail.com>
2024-02-23 16:40:54 +0800

    完善dockerfile和shell

--- build/build.sh
+++ build/build.sh
@@ -13,12 +13,14 @@ mkdir -p ./data
 echo "cd /GoTrace && GOOS=linux GOARCH=${ARCH} go build -o GoTrace . && cp -arf ./config /data && cp GoTrace /data" > ./data/build.sh
 chmod +x ./data/build.sh
 
-# 使用构建的镜像在 Docker 容器中运行
-docker run -itd --rm -v $(pwd)/data:/data --net=host gotrace:build
+# 使用构建的镜像在 Docker 容器中运行，并等待容器执行完毕
+CONTAINER_ID=$(docker run -itd --rm -v $(pwd)/data:/data --net=host gotrace:build)
+docker wait $CONTAINER_ID
 
 # 清理工作
 docker rmi -f gotrace:build
 rm -rf ./data/build.sh
+
 
 # 打印成功消息
 echo "恭喜！编译成功。在 ./data 目录中找到二进制文件和相应的配置文件。"
