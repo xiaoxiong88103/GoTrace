4bb7253d07c24c7da9bebb8d84547e6bba0b401c
“xiaoxiong88103” <xiaoxiong88103@hotmail.com>
2024-02-23 16:29:10 +0800

    完善dockerfile和shell

--- build/build.sh
+++ build/build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-docker build -t gotrace:build --build-arg GOARCH=$($1) .
+docker build -t gotrace:build .
 
 mkdir ./data
-
+echo "go build GOOS=linux GOARCH="$($1) + "-o GoTrace .">> ./data/build.sh
 docker run -itd --rm -v $(pwd)/data:/data --net=host gotrace:build
 
 docker rmi -f gotrace:build
+rm -rf ./data/build.sh
 echo "恭喜你编译成功在./data目录下就是编译好的二进制文件和config对应的"
