fa5e8a84377337a1db47224f647dd2ea8aab484a
“xiaoxiong88103” <xiaoxiong88103@hotmail.com>
2024-02-23 14:23:59 +0800

    完善dockerfile和shell

--- build/build.sh
+++ build/build.sh
@@ -31,3 +31,12 @@ done
 for ARCH in "$@"; do
     ./build.sh "$ARCH"
 done
+
+docker build -t gotrace:build --build-arg .
+
+mkdir ./data
+
+docker run -itd --rm -v $(pwd)/data:/data --net=host gotrace:build
+
+docker rmi -f gotrace:build
+echo "恭喜你编译成功在./data目录下就是编译好的二进制文件和config对应的"
