3865385e8097d5f80524725dcf02b55fc73cabe0
“xiaoxiong88103” <xiaoxiong88103@hotmail.com>
2024-02-23 12:58:38 +0800

    增加了dockerfile 和build.sh 来完成自动化的构建

---
+++ build/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+docker build -t GoTrace:build .
+
+mkdir data
+
+docker run -it --rm -v $(pwd):/data --net=host GoTrace:build
+
+docker rmi -f GoTrace:build
+echo "恭喜你编译成功在./data目录下就是编译好的二进制文件和config对应的"
